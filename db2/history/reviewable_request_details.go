5b4c0d7ba545692bd7b385bfefc752678fd39b70
Dmitriy Haidashenko <d.s.gaydashenko@gmail.com>
2017-12-19 15:24:39 +0200

    Reviewer details for withdrawal request

--- db2/history/reviewable_request_details.go
+++ db2/history/reviewable_request_details.go
@@ -33,11 +33,12 @@ type IssuanceRequest struct {
 }
 
 type WithdrawalRequest struct {
-	BalanceID       string `json:"balance_id"`
-	Amount          string `json:"amount"`
-	FixedFee        string `json:"fixed_fee"`
-	PercentFee      string `json:"percent_fee"`
-	ExternalDetails string `json:"external_details"`
-	DestAssetCode   string `json:"dest_asset_code"`
-	DestAssetAmount string `json:"dest_asset_amount"`
+	BalanceID       string                 `json:"balance_id"`
+	Amount          string                 `json:"amount"`
+	FixedFee        string                 `json:"fixed_fee"`
+	PercentFee      string                 `json:"percent_fee"`
+	ExternalDetails string                 `json:"external_details"`
+	DestAssetCode   string                 `json:"dest_asset_code"`
+	DestAssetAmount string                 `json:"dest_asset_amount"`
+	ReviewerDetails map[string]interface{} `json:"reviewer_details"`
 }
