7c75df40677e42baf04b9b94f29536a36b356ce5
Oleg <anfimov.dev@gmail.com>
2018-01-13 16:45:35 +0200

    Add Participants

--- resource/operations/main_test.go
+++ resource/operations/main_test.go
@@ -7,6 +7,8 @@ import (
 
 	"encoding/json"
 
+	"fmt"
+
 	"github.com/guregu/null"
 	"github.com/stretchr/testify/assert"
 	"gitlab.com/swarmfund/go/xdr"
@@ -22,7 +24,7 @@ func getOperation(opType xdr.OperationType, details string) history.Operation {
 	return history.Operation{
 		TotalOrderID:     db2.TotalOrderID{ID: 231928242177},
 		TransactionID:    231928242176,
-		TransactionHash:  "hashy",
+		TransactionHash:  "73559b4bda9057acc6566da0e3f0e2a7eab6f7742df9ffe86a3a5cef6ef081cd",
 		ApplicationOrder: 1,
 		Type:             opType,
 		DetailsString:    null.NewString(details, true),
@@ -33,15 +35,32 @@ func getOperation(opType xdr.OperationType, details string) history.Operation {
 	}
 }
 
+func getParticipants() []*history.Participant {
+	participants := make([]*history.Participant, 3)
+	for i := range participants {
+		participants[i] = &history.Participant{
+			OperationID: int64(1234567 + i),
+			AccountID:   "GD7AHJHCDSQI6LVMEJEE2FTNCA2LJQZ4R64GUI3PWANSVEO4GEOWB63" + fmt.Sprintf("%v", i),
+			BalanceID:   "GD7AHJHCDSQI6LVMEJEE2FTNCA2LJQZ4R64GUI3PWANSVEO4GEOWB63" + fmt.Sprintf("%v", i),
+			Nickname:    "Nickname" + fmt.Sprintf("%v", i),
+			Email:       "email" + fmt.Sprintf("%v", i) + "@test.com",
+			Mobile:      "38012123456" + fmt.Sprintf("%v", i),
+			Details:     []byte{1, 2, 3, 4, 5, byte(i)},
+			UserType:    "general",
+			Effects:     nil, //TODO ask about it
+		}
+	}
+	return participants
+}
+
 //This test check only JSON output, he didn't check state of operation
-func TestNewCreateAccountOperation(t *testing.T) {
+func TestNew(t *testing.T) {
 	ctx := context.TODO()
-	// TODO helper for participants
-	participant := []*history.Participant{}
+	participants := getParticipants()
 
 	operation := getOperation(xdr.OperationTypeCreateAccount, `{"funder": "GD7AHJHCDSQI6LVMEJEE2FTNCA2LJQZ4R64GUI3PWANSVEO4GEOWB636", "account": "GBCADUBIWUSO5HZF7YZUC42Z6DWSCJH2GQQUEJH6HD6OE4TSYU46F7XT", "account_type": 2}`)
 
-	result, err := New(ctx, operation, participant, false)
+	result, err := New(ctx, operation, participants, false)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -50,34 +69,55 @@ func TestNewCreateAccountOperation(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	expected := `{
-		  "_links": {
-			"self": {
-			  "href": "/operations/231928242177"
-			},
-			"transaction": {
-			  "href": "/transactions/hashy"
-			},
-			"succeeds": {
-			  "href": "/effects?order=desc&cursor=231928242177"
-			},
-			"precedes": {
-			  "href": "/effects?order=asc&cursor=231928242177"
-			}
-		  },
-		  "id": "231928242177",
-		  "paging_token": "231928242177",
-		  "transaction_id": "231928242176",
-		  "source_account": "GD7AHJHCDSQI6LVMEJEE2FTNCA2LJQZ4R64GUI3PWANSVEO4GEOWB636",
-		  "type": "create_account",
-		  "type_i": 0,
-		  "state_i": 2,
-		  "state": "success",
-		  "identifier": "4",
-		  "ledger_close_time": "2018-01-11T13:51:15Z",
-		  "funder": "GD7AHJHCDSQI6LVMEJEE2FTNCA2LJQZ4R64GUI3PWANSVEO4GEOWB636",
-		  "account": "GBCADUBIWUSO5HZF7YZUC42Z6DWSCJH2GQQUEJH6HD6OE4TSYU46F7XT",
-		  "account_type": 2
+		 "_links": {
+		"self": {
+		  "href": "/operations/231928242177"
+		},
+		"transaction": {
+		  "href": "/transactions/73559b4bda9057acc6566da0e3f0e2a7eab6f7742df9ffe86a3a5cef6ef081cd"
+		},
+		"succeeds": {
+		  "href": "/effects?order=desc&cursor=231928242177"
+		},
+		"precedes": {
+		  "href": "/effects?order=asc&cursor=231928242177"
+		}
+	  },
+	  "id": "231928242177",
+	  "paging_token": "231928242177",
+	  "transaction_id": "231928242176",
+	  "source_account": "GD7AHJHCDSQI6LVMEJEE2FTNCA2LJQZ4R64GUI3PWANSVEO4GEOWB636",
+	  "type": "create_account",
+	  "type_i": 0,
+	  "state_i": 2,
+	  "state": "success",
+	  "identifier": "4",
+	  "ledger_close_time": "2018-01-11T13:51:15Z",
+	  "participants": [
+		{
+		  "account_id": "GD7AHJHCDSQI6LVMEJEE2FTNCA2LJQZ4R64GUI3PWANSVEO4GEOWB630",
+		  "balance_id": "GD7AHJHCDSQI6LVMEJEE2FTNCA2LJQZ4R64GUI3PWANSVEO4GEOWB630",
+		  "email": "email0@test.com",
+		  "nickname": "Nickname0"
+		},
+		{
+		  "account_id": "GD7AHJHCDSQI6LVMEJEE2FTNCA2LJQZ4R64GUI3PWANSVEO4GEOWB631",
+		  "balance_id": "GD7AHJHCDSQI6LVMEJEE2FTNCA2LJQZ4R64GUI3PWANSVEO4GEOWB631",
+		  "email": "email1@test.com",
+		  "nickname": "Nickname1"
+		},
+		{
+		  "account_id": "GD7AHJHCDSQI6LVMEJEE2FTNCA2LJQZ4R64GUI3PWANSVEO4GEOWB632",
+		  "balance_id": "GD7AHJHCDSQI6LVMEJEE2FTNCA2LJQZ4R64GUI3PWANSVEO4GEOWB632",
+		  "email": "email2@test.com",
+		  "nickname": "Nickname2"
+		}
+	  ],
+	  "funder": "GD7AHJHCDSQI6LVMEJEE2FTNCA2LJQZ4R64GUI3PWANSVEO4GEOWB636",
+	  "account": "GBCADUBIWUSO5HZF7YZUC42Z6DWSCJH2GQQUEJH6HD6OE4TSYU46F7XT",
+	  "account_type": 2
 	}`
 	assert.JSONEq(t, expected, string(marshalRes))
 }
