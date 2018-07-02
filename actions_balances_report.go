a8c78ea0712b4a964bc521ac150aa1bb2264c32f
Alexandr Gulak <lx.gulak@gmail.com>
2018-07-02 21:17:21 +0300

    Fix review issues

--- actions_balances_report.go
+++ actions_balances_report.go
@@ -3,20 +3,15 @@ package horizon
 import (
 	"gitlab.com/swarmfund/horizon/exchange"
 	"gitlab.com/swarmfund/horizon/render/hal"
+	"gitlab.com/swarmfund/horizon/render/problem"
+	"gitlab.com/tokend/regources"
 )
-
-type Records struct {
-	TotalZeroBalanceAccountsCount     int `json:"total_zero_balance_accounts_count"`
-	TotalPositiveBalanceAccountsCount int `json:"total_positive_balance_accounts_count"`
-	TotalAboveThresholdAccountsCount  int `json:"total_above_threshold_accounts_count"`
-	TotalBelowThresholdAccountsCount  int `json:"total_below_threshold_accounts_count"`
-}
 
 type BalancesReportAction struct {
 	Action
 	threshold int64
 	assetType string
-	Records   Records `json:"records"`
+	Records   regources.BalancesReport
 }
 
 func (action *BalancesReportAction) JSON() {
@@ -31,7 +26,7 @@ func (action *BalancesReportAction) JSON() {
 
 func (action *BalancesReportAction) loadParams() {
 	action.threshold = action.GetInt64("threshold")
-	action.assetType = action.GetString("asset_type")
+	action.assetType = action.GetString("asset_code")
 }
 
 func (action *BalancesReportAction) loadRecords() {
@@ -39,13 +34,17 @@ func (action *BalancesReportAction) loadRecords() {
 	balances, err := action.CoreQ().Balances().NonZero().Select()
 	if err != nil {
 		action.Log.WithError(err).Warn("failed to filter non-zero balances")
+		action.Err = &problem.ServerError
+		return
 	}
 
-	USDAmountFromAccount := make(map[string]int64)
+	AmountFromAccount := make(map[string]int64)
 
 	converter, err := exchange.NewConverter(action.CoreQ())
 	if err != nil {
 		action.Log.Warn("failed to create converter")
+		action.Err = &problem.ServerError
+		return
 	}
 
 	// group balances by account and assign converted amounts
@@ -53,20 +52,24 @@ func (action *BalancesReportAction) loadRecords() {
 		amount, err := converter.TryToConvertWithOneHop(balance.Amount+balance.Locked, balance.Asset, action.assetType)
 		if err != nil {
 			action.Log.Warn("failed to convert to asset type, skipping")
+			action.Err = &problem.ServerError
+			return
 		}
 		if amount == nil {
 			action.Log.WithField("base", balance.Asset).WithField("quote", action.assetType).Warn("the asset is unconvertable, skipping")
+			action.Err = &problem.ServerError
+			return
 		} else {
-			USDAmountFromAccount[balance.AccountID] += *amount
+			AmountFromAccount[balance.AccountID] += *amount
 		}
 	}
 
 	// filter and count by threshold
-	for account := range USDAmountFromAccount {
-		if USDAmountFromAccount[account] < action.threshold {
-			action.Records.TotalBelowThresholdAccountsCount += 1
+	for account := range AmountFromAccount {
+		if AmountFromAccount[account] < action.threshold {
+			action.Records.TotalAccountsCount.BelowThreshold += 1
 		} else {
-			action.Records.TotalAboveThresholdAccountsCount += 1
+			action.Records.TotalAccountsCount.AboveThreshold += 1
 		}
 	}
 
@@ -74,6 +77,8 @@ func (action *BalancesReportAction) loadRecords() {
 	zeroBalances, err := action.CoreQ().Balances().Zero().Select()
 	if err != nil {
 		action.Log.WithError(err).Warn("failed to filter zero balances")
+		action.Err = &problem.ServerError
+		return
 	}
 
 	isZeroAmountAccount := make(map[string]bool)
@@ -83,6 +88,6 @@ func (action *BalancesReportAction) loadRecords() {
 		isZeroAmountAccount[balance.AccountID] = true
 	}
 
-	action.Records.TotalZeroBalanceAccountsCount = len(isZeroAmountAccount)
-	action.Records.TotalPositiveBalanceAccountsCount = len(USDAmountFromAccount)
+	action.Records.TotalAccountsCount.ZeroBalance = len(isZeroAmountAccount)
+	action.Records.TotalAccountsCount.PositiveBalance = len(AmountFromAccount)
 }
