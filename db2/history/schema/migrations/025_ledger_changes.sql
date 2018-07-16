0834158ab441ff6815e979d1d282e41c20c3bf60
Artem <artemkaskriabin@gmail.com>
2018-07-16 15:58:24 +0300

    Delete check in ledger changes table

--- db2/history/schema/migrations/025_ledger_changes.sql
+++ db2/history/schema/migrations/025_ledger_changes.sql
@@ -2,9 +2,9 @@
 
 CREATE TABLE history_ledger_changes
 (
-  tx_id         BIGINT    NOT NULL CHECK (tx_id >= 0),
-  op_id         BIGINT    NOT NULL CHECK (op_id >= 0),
-  order_number  INT       NOT NULL CHECK (order_number >= 0),
+  tx_id         BIGINT    NOT NULL,
+  op_id         BIGINT    NOT NULL,
+  order_number  INT       NOT NULL,
   effect        INT       NOT NULL,
   entry_type    INT       NOT NULL,
   payload       TEXT      NOT NULL
