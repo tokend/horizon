e2a27f5fe8806eebee1723a6368daca094ba610e
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2018-07-25 15:32:39 +0300

    Add ledger change sort within op

--- db2/history/ledger_changes_q.go
+++ db2/history/ledger_changes_q.go
@@ -102,7 +102,8 @@ func (q *LedgerChangesQ) ByTransactionIDs(page db2.PageQuery, entryTypes []int, 
 	selectTxIDs, q.Err = page.ApplyTo(selectTxIDs, "hlc.tx_id")
 
 	// extend order by to preserve ledger changes order within tx
-	selectTxIDs = selectTxIDs.OrderBy("hlc.op_id")
+	// `op_id` will sort operations and `order_number` is index of change in op
+	selectTxIDs = selectTxIDs.OrderBy("hlc.op_id", "hlc.order_number")
 
 	if q.Err != nil {
 		q.Err = errors.Wrap(q.Err, "failed to get paging params")
