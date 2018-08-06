6c22bef57c34d818ed57ae893ce3f25d5f33f386
Yevhenii Luhanskyi <y.luhanskyi@gmail.com>
2018-08-06 15:48:21 +0300

    Update vendor

--- vendor/gitlab.com/tokend/regources/info.go
+++
@@ -1,7 +0,0 @@
-package regources
-
-type Info struct {
-	Passphrase         string `json:"network_passphrase"`
-	MasterAccountID    string `json:"master_account_id"`
-	TXExpirationPeriod int64  `json:"tx_expiration_period"`
-}
