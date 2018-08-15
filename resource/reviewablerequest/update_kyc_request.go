ae120152efb99b1dc1c12496937775a5a1e82f08
Andrew Stepko <andrew.stepko@gmail.com>
2018-08-15 22:19:33 +0300

    Change type AccountTypeToSet struct {

--- resource/reviewablerequest/update_kyc_request.go
+++ resource/reviewablerequest/update_kyc_request.go
@@ -10,7 +10,10 @@ func PopulateUpdateKYCRequest(histRequest history.UpdateKYCRequest) (
 ) {
 	r = &regources.UpdateKYCRequest{}
 	r.AccountToUpdateKYC = histRequest.AccountToUpdateKYC
-	r.AccountTypeToSet = int32(histRequest.AccountTypeToSet)
+	r.AccountTypeToSet = regources.AccountTypeToSet{
+		Int: int(histRequest.AccountTypeToSet),
+		String: histRequest.AccountTypeToSet.ShortString(),
+	}
 	r.KYCLevel = histRequest.KYCLevel
 	r.KYCData = histRequest.KYCData
 	r.AllTasks = histRequest.AllTasks
