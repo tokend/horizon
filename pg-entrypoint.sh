43ec1579ec23c19af8d63ea4201718da2cbc965a
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2018-10-16 21:19:58 +0300

    Fix pg healtcheck

--- pg-entrypoint.sh
+++ pg-entrypoint.sh
@@ -3,7 +3,7 @@ set -eax
 
 pg-entrypoint.sh postgres &
 
-while ! pg_isready -U$POSTGRES_USER > /dev/null 2> /dev/null; do
+while ! pg_isready -U$POSTGRES_USER -hlocalhost > /dev/null 2> /dev/null; do
     echo "postgres not ready yet"
     sleep 3
 done
