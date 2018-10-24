64351db27a0a9149a83c9e8dd1ee551d8a3a542a
Skriabin <artemkaskriabin@gmail.com>
2018-10-24 17:14:53 +0300

    Remove isBuy and state filters

--- actions_history_offer_index.go
+++ actions_history_offer_index.go
@@ -1,22 +1,10 @@
 package horizon
 
 import (
-	"fmt"
-
 	"gitlab.com/tokend/horizon/db2"
 	"gitlab.com/tokend/horizon/render/hal"
 	"gitlab.com/tokend/horizon/render/problem"
 	"gitlab.com/tokend/horizon/resource"
-)
-
-type OfferState int64
-
-const (
-	OfferStateAny OfferState = iota
-	OfferStateNoMatches
-	OfferStatePartiallyMatched
-	OfferStateFullyMatched
-	OfferStateCanceled
 )
 
 type HistoryOfferIndexAction struct {
@@ -24,8 +12,6 @@ type HistoryOfferIndexAction struct {
 	OwnerID      string
 	BaseAsset    string
 	QuoteAsset   string
-	IsBuy        *bool
-	OfferState   OfferState
 	PagingParams db2.PageQuery
 	Page         hal.Page
 }
@@ -48,54 +34,25 @@ func (action *HistoryOfferIndexAction) checkAllowed() {
 }
 
 func (action *HistoryOfferIndexAction) loadParams() {
-	action.OwnerID = action.GetString("owner_id")
-	action.BaseAsset = action.GetString("base_asset")
-	action.QuoteAsset = action.GetString("quote_asset")
-	action.IsBuy = action.GetOptionalBool("is_buy")
-
-	action.OfferState = OfferState(action.GetInt64("state"))
+	action.OwnerID = action.GetNonEmptyString("owner_id")
+	action.BaseAsset = action.GetNonEmptyString("base_asset")
+	action.QuoteAsset = action.GetNonEmptyString("quote_asset")
 
 	action.PagingParams = action.GetPageQuery()
 	action.Page.Filters = map[string]string{
 		"owner_id":    action.OwnerID,
 		"base_asset":  action.BaseAsset,
 		"quote_asset": action.QuoteAsset,
-		"is_buy":      action.GetString("is_buy"),
 	}
 }
 
 func (action *HistoryOfferIndexAction) loadRecords() {
-	q := action.HistoryQ().Offers()
-	if action.OwnerID != "" {
-		q = q.ForOwnerID(action.OwnerID)
-	}
-	if action.BaseAsset != "" {
-		q = q.ForBase(action.BaseAsset)
-	}
-	if action.QuoteAsset != "" {
-		q = q.ForQuote(action.QuoteAsset)
-	}
-	if action.IsBuy != nil {
-		q = q.ForIsBuy(*action.IsBuy)
-	}
-
-	switch action.OfferState {
-	case OfferStateAny:
-		// all offers are appropriate
-	case OfferStateNoMatches:
-		q = q.NoMatches().Canceled(false)
-	case OfferStatePartiallyMatched:
-		q = q.PartiallyMatched().Canceled(false)
-	case OfferStateFullyMatched:
-		q = q.FullyMatched()
-	case OfferStateCanceled:
-		q = q.Canceled(true)
-	default:
-		action.SetInvalidField("state", fmt.Errorf("invalid value %d", action.OfferState))
-		return
-	}
-
-	historyOffers, err := q.Page(action.PagingParams).Select()
+	historyOffers, err := action.HistoryQ().Offers().
+		ForOwnerID(action.OwnerID).
+		ForBase(action.BaseAsset).
+		ForQuote(action.QuoteAsset).
+		Page(action.PagingParams).
+		Select()
 	if err != nil {
 		action.Log.WithError(err).Error("failed to select history offers")
 		action.Err = &problem.ServerError
