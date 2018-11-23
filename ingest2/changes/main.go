716f0ec82142fbd2247eb49c35aeaf3b7fe48b79
Oleksii Khramov <khramoff.al@gmail.com>
2018-11-23 13:41:33 +0200

    Added consuming of changes and routing thru changes processors

--- ingest2/changes/main.go
+++ ingest2/changes/main.go
@@ -3,6 +3,8 @@ package changes
 import (
 	"time"
 
+	"gitlab.com/distributed_lab/logan/v3/errors"
+
 	"gitlab.com/tokend/go/xdr"
 )
 
@@ -11,4 +13,86 @@ type LedgerChange struct {
 	LedgerCloseTime time.Time
 	LedgerChange    xdr.LedgerEntryChange
 	Operation       *xdr.Operation
+}
+
+var (
+	balances           = balanceChanges{}
+	contracts          = contractChanges{}
+	reviewableRequests = reviewableRequestChanges{}
+	sales              = saleChanges{}
+)
+
+func (l LedgerChange) Created() error {
+	switch l.LedgerChange.Created.Data.Type {
+	case xdr.LedgerEntryTypeBalance:
+		err := balances.Created(l)
+		if err != nil {
+			return errors.Wrap(err, "failed to process balance created event")
+		}
+		return nil
+	case xdr.LedgerEntryTypeContract:
+		err := contracts.Created(l)
+		if err != nil {
+			return errors.Wrap(err, "failed to process contract created event")
+		}
+		return nil
+	case xdr.LedgerEntryTypeReviewableRequest:
+		err := reviewableRequests.Created(l)
+		if err != nil {
+			return errors.Wrap(err, "failed to process reviewable request created event")
+		}
+		return nil
+	case xdr.LedgerEntryTypeSale:
+		err := sales.Created(l)
+		if err != nil {
+			return errors.Wrap(err, "failed to process sale created event")
+		}
+		return nil
+	default:
+		return errors.Errorf("Unrecognized ledger entry type created: %d", l.LedgerChange.Created.Data.Type)
+	}
+}
+
+func (l LedgerChange) Updated() error {
+	switch l.LedgerChange.Updated.Data.Type {
+	case xdr.LedgerEntryTypeContract:
+		err := contracts.Updated(l)
+		if err != nil {
+			return errors.Wrap(err, "failed to process contract updated event")
+		}
+		return nil
+	case xdr.LedgerEntryTypeReviewableRequest:
+		err := reviewableRequests.Updated(l)
+		if err != nil {
+			return errors.Wrap(err, "failed to process reviewable request updated event")
+		}
+		return nil
+	case xdr.LedgerEntryTypeSale:
+		err := sales.Updated(l)
+		if err != nil {
+			return errors.Wrap(err, "failed to process sale updated event")
+		}
+		return nil
+	default:
+		return errors.Errorf("Unrecognized ledger entry type updated: %d", l.LedgerChange.Updated.Data.Type)
+	}
+}
+
+func (l LedgerChange) Deleted() error {
+	switch l.LedgerChange.Removed.Type {
+	case xdr.LedgerEntryTypeContract:
+		err := contracts.Deleted(l)
+		if err != nil {
+			return errors.Wrap(err, "failed to process contract deleted event")
+		}
+		return nil
+	case xdr.LedgerEntryTypeReviewableRequest:
+		err := reviewableRequests.Deleted(l)
+		if err != nil {
+			return errors.Wrap(err, "failed to process reviewable request deleted event")
+		}
+		return nil
+	default:
+		return errors.Errorf("Unrecognized ledger entry type deleted: %d", l.LedgerChange.Removed.Type)
+	}
 }
--- ingest2/ledger_consumer.go
+++ ingest2/ledger_consumer.go
@@ -2,11 +2,16 @@ package ingest2
 
 import (
 	"context"
+	"time"
+
+	"gitlab.com/tokend/go/xdr"
+
+	"gitlab.com/tokend/horizon/ingest2/changes"
+
 	"gitlab.com/distributed_lab/logan/v3"
 	"gitlab.com/distributed_lab/logan/v3/errors"
 	"gitlab.com/tokend/horizon/db2"
 	"gitlab.com/tokend/horizon/db2/history2"
-	"time"
 )
 
 type ledgerStorage interface {
@@ -14,7 +19,6 @@ type ledgerStorage interface {
 }
 
 type operation struct {
-
 }
 
 type operationConsumer interface {
@@ -22,7 +26,22 @@ type operationConsumer interface {
 }
 
 type ledgerChange struct {
+	changes.LedgerChange
+}
 
+func (c *ledgerChange) Consume(ledgerChange) error {
+	switch c.LedgerChange.LedgerChange.Type {
+	case xdr.LedgerEntryChangeTypeCreated:
+		return c.LedgerChange.Created()
+	case xdr.LedgerEntryChangeTypeUpdated:
+		return c.LedgerChange.Updated()
+	case xdr.LedgerEntryChangeTypeRemoved:
+		return c.LedgerChange.Deleted()
+	case xdr.LedgerEntryChangeTypeState:
+		return nil
+	default:
+		return errors.Errorf("Unrecognized ledger entry change type: %d", c.LedgerChange.LedgerChange.Type)
+	}
 }
 
 type ledgerChangesConsumer interface {
@@ -32,14 +51,14 @@ type ledgerChangesConsumer interface {
 type ledgerConsumer struct {
 	log           *logan.Entry
 	ledgerStorage ledgerStorage
-	lcConsumer ledgerChangesConsumer
-	opConsumer operationConsumer
+	lcConsumer    ledgerChangesConsumer
+	opConsumer    operationConsumer
 }
 
 func (c *ledgerConsumer) Consume(ctx context.Context, bundle ledgerBundle) error {
 	ledgerGlobalOpSeq := int32(0)
 	fields := logan.F{
-		"ledger_seq":        bundle.Sequence,
+		"ledger_seq": bundle.Sequence,
 	}
 	for txSeq, tx := range bundle.Transactions {
 		fields = fields.Add("tx_seq", txSeq)
@@ -49,7 +68,14 @@ func (c *ledgerConsumer) Consume(ctx context.Context, bundle ledgerBundle) error
 
 			for lcSeq, lc := range operationsMeta[opSeq].Changes {
 				fields = fields.Add("ledger_change_seq", lcSeq)
-				err := c.lcConsumer.Consume(ledgerChange{})
+				err := c.lcConsumer.Consume(ledgerChange{
+					changes.LedgerChange{
+						LedgerChange:    lc,
+						LedgerCloseTime: time.Unix(bundle.Header.CloseTime, 0),
+						LedgerSeq:       bundle.Sequence,
+						Operation:       &op,
+					},
+				})
 				if err != nil {
 					return errors.Wrap(err, "failed to process ledger change", fields)
 				}
@@ -59,8 +85,6 @@ func (c *ledgerConsumer) Consume(ctx context.Context, bundle ledgerBundle) error
 			if err != nil {
 				return errors.Wrap(err, "failed to process operation", fields)
 			}
-
-
 
 			ledgerGlobalOpSeq++
 		}
