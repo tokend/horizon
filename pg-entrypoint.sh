6a31c292ff65cb9bf0b6067d765aec69364768d5
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2018-12-07 13:33:11 +0200

    Backport entrypoints

--- pg-entrypoint.sh
+++ pg-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/dumb-init /bin/bash
 set -eax
 
-pg-entrypoint.sh postgres &
+POSTGRES_PORT=${POSTGRES_PORT:-5432}
+pg-entrypoint.sh "postgres -p $POSTGRES_PORT" &
 
-while ! pg_isready -U$POSTGRES_USER -hlocalhost > /dev/null 2> /dev/null; do
+while ! pg_isready -U$POSTGRES_USER -hlocalhost -p$POSTGRES_PORT > /dev/null 2> /dev/null; do
     echo "postgres not ready yet"
     sleep 3
 done
