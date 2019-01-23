aa894e8617ab7bb09dbfecc508f5a625223e23c3
Vladislav <logvinenko.js@gmail.com>
2019-01-23 21:54:11 +0200

    Join assets instead of querying 2 times for /assets_pairs/:id

--- web_v2/handlers/get_asset_pair.go
+++ web_v2/handlers/get_asset_pair.go
@@ -52,7 +52,17 @@ type getAssetPairHandler struct {
 
 // GetAssetPair returns asset pair with related resources
 func (h *getAssetPairHandler) GetAssetPair(request *requests.GetAssetPair) (*regources.AssetPairResponse, error) {
-	coreAssetPair, err := h.AssetPairsQ.GetByBaseAndQuote(request.BaseAsset, request.QuoteAsset)
+	q := h.AssetPairsQ
+
+	if request.ShouldInclude(requests.IncludeTypeAssetPairBaseAsset) {
+		q = q.WithBaseAsset()
+	}
+
+	if request.ShouldInclude(requests.IncludeTypeAssetPairQuoteAsset) {
+		q = q.WithQuoteAsset()
+	}
+
+	coreAssetPair, err := q.GetByBaseAndQuote(request.BaseAsset, request.QuoteAsset)
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to get asset pair by base and quote")
 	}
@@ -70,33 +80,18 @@ func (h *getAssetPairHandler) GetAssetPair(request *requests.GetAssetPair) (*reg
 	response.Data.Relationships.QuoteAsset = quoteAssetKey.AsRelation()
 
 	if request.ShouldInclude(requests.IncludeTypeAssetPairBaseAsset) {
-		baseAsset, err := h.getAsset(coreAssetPair.Base)
-		if err != nil {
-			return nil, errors.Wrap(err, "Failed to get base asset")
-		}
+		coreBaseAsset := coreAssetPair.BaseAsset
+		baseAsset := resources.NewAsset(*coreBaseAsset)
 
-		response.Included.Add(baseAsset)
+		response.Included.Add(&baseAsset)
 	}
 
 	if request.ShouldInclude(requests.IncludeTypeAssetPairQuoteAsset) {
-		quoteAsset, err := h.getAsset(coreAssetPair.Quote)
-		if err != nil {
-			return nil, errors.Wrap(err, "Failed to get quote asset")
-		}
+		coreQuoteAsset := coreAssetPair.QuoteAsset
+		quoteAsset := resources.NewAsset(*coreQuoteAsset)
 
-		response.Included.Add(quoteAsset)
+		response.Included.Add(&quoteAsset)
 	}
 
 	return response, nil
-}
-
-func (h *getAssetPairHandler) getAsset(code string) (*regources.Asset, error) {
-	coreAsset, err := h.AssetsQ.GetByCode(code)
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to get asset by code")
-	}
-
-	asset := resources.NewAsset(*coreAsset)
-
-	return &asset, nil
 }
