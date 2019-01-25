c295fbbf0ae8294ff58c1fcd1f95367ddc3f26e1
Dmitriy Haidashenko <dh@distributedlab.com>
2019-01-25 15:07:28 +0200

    Fixed null arrays

--- vendor/gitlab.com/tokend/regources/v2/participant_effect.go
+++ vendor/gitlab.com/tokend/regources/v2/participant_effect.go
@@ -1,10 +1,21 @@
 package regources
 
+import "encoding/json"
+
 //ParticipantEffectsResponse - response for history and movements handlers
 type ParticipantEffectsResponse struct {
 	Links    *Links              `json:"links"`
 	Data     []ParticipantEffect `json:"data"`
 	Included Included            `json:"included"`
+}
+
+func (r ParticipantEffectsResponse) MarshalJSON() ([]byte, error) {
+	if r.Data == nil {
+		r.Data = []ParticipantEffect{}
+	}
+
+	type temp ParticipantEffectsResponse
+	return json.Marshal(temp(r))
 }
 
 //ParticipantEffect - represent account effected by operation
