ad140e7fee30d8bcc7befaf75a51e3733ffd261d
Vladislav <logvinenko.js@gmail.com>
2019-02-06 21:58:39 +0200

    Merge branch 'hotfixes/prune-unused-packages'

--- vendor/gitlab.com/tokend/regources/v2/relation.go
+++ vendor/gitlab.com/tokend/regources/v2/relation.go
@@ -1,7 +1,5 @@
 package regources
 
-import "encoding/json"
-
 type Relation struct {
 	Links *Links `json:"links,omitempty"`
 	Data  *Key   `json:"data,omitempty"`
@@ -10,13 +8,4 @@ type Relation struct {
 type RelationCollection struct {
 	Links *Links `json:"links,omitempty"`
 	Data  []Key  `json:"data,omitempty"`
-}
-
-func (r RelationCollection) MarshalJSON() ([]byte, error) {
-	if r.Data == nil {
-		r.Data = []Key{}
-	}
-
-	type temp RelationCollection
-	return json.Marshal(temp(r))
 }
