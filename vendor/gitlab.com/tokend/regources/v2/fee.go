a774b93226d9f1af175eff307992acd26ffec2f7
Hennadii <hennadii.ponedielnikov@gmail.com>
2019-02-21 17:56:09 +0200

    Revert "updated regources"
    This reverts commit ac63b520b7448c37e4530e432f271d7f5bcc897a.

--- vendor/gitlab.com/tokend/regources/v2/fee.go
+++ vendor/gitlab.com/tokend/regources/v2/fee.go
@@ -3,8 +3,8 @@ package regources
 // Fee - describes fee happened on balance. Direction of fee depends on the operation (depending on effect might be
 // charged, locked, unlocked, for all incoming effects but unlocked it's always charged)
 type Fee struct {
-	Fixed             Amount `json:"fixed"`
-	CalculatedPercent Amount `json:"calculated_percent"`
+	Fixed             Amount `json:"fixed,omitempty"`
+	CalculatedPercent Amount `json:"calculated_percent,omitempty"`
 }
 
 type FeeRecord struct {
