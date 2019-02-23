656d8a61a95fbec8fd095b6ed695c8dcc1c8a173
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2019-02-23 15:50:09 +0200

    Fix

--- doorman/main.go
+++ doorman/main.go
@@ -31,8 +31,8 @@ func (q *SignersQ) Signers(address string) ([]resources.Signer, error) {
 	result := make([]resources.Signer, 0, len(signers))
 	for _, signer := range signers {
 		result = append(result, resources.Signer{
-			PublicKey: signer.PublicKey,
-			AccountID: signer.AccountID,
+			// FIXME with it properly in doorman as well.
+			AccountID: signer.PublicKey,
 			Weight:    int(signer.Weight),
 			Role:      signer.RoleID,
 			Identity:  signer.Identity,
