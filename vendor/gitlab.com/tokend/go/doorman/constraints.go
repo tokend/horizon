f277a5f1fa5421e5a1d6fd4dcf81d9acdecb2bd4
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2019-02-23 15:35:25 +0200

    Revert "Debug logs"
    This reverts commit 59b933a4af405026a9e633332455ef998bd1d42f.

--- vendor/gitlab.com/tokend/go/doorman/constraints.go
+++ vendor/gitlab.com/tokend/go/doorman/constraints.go
@@ -1,7 +1,6 @@
 package doorman
 
 import (
-	"fmt"
 	"net/http"
 
 	"gitlab.com/tokend/go/signcontrol"
@@ -11,7 +10,6 @@ func SignerOf(address string) SignerConstraint {
 	return func(r *http.Request, doorman Doorman) error {
 		signer, err := signcontrol.CheckSignature(r)
 		if err != nil {
-			fmt.Printf("%s: signature is not valid: %v\n", r.URL.String(), err)
 			return err
 		}
 
@@ -20,17 +18,14 @@ func SignerOf(address string) SignerConstraint {
 		}
 		signers, err := doorman.AccountSigners(address)
 		if err != nil {
-			fmt.Printf("%s: failed to get account signers: %v\n", r.URL.String(), err)
 			return err
 		}
 
 		for _, accountSigner := range signers {
-			fmt.Printf("%s: %s %s %d \n", r.URL.String(), accountSigner.AccountID, signer, accountSigner.Weight)
 			if accountSigner.AccountID == signer && accountSigner.Weight > 0 {
 				return nil
 			}
 		}
-		fmt.Printf("%s: you die\n", r.URL.String())
 		return signcontrol.ErrNotAllowed
 	}
 }
