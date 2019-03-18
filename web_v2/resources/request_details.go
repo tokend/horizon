3ed6a0f18737716e2206e00b626b7326bada35f9
Skriabin <artem.skriabin@distributedlab.com>
2019-03-18 12:32:52 +0200

    Update docs

--- web_v2/resources/request_details.go
+++ web_v2/resources/request_details.go
@@ -88,7 +88,7 @@ func newAtomicSwapRequest(id int64, details history2.CreateAtomicSwapRequest) *r
 			BaseAmount: regources.Amount(details.BaseAmount),
 		},
 		Relationships: regources.CreateAtomicSwapRequestRelations{
-			Bid:        regources.NewKeyInt64(int64(details.BidID), regources.TypeAtomicSwapBid).AsRelation(),
+			Bid:        regources.NewKeyInt64(int64(details.BidID), regources.TypeAtomicSwapBids).AsRelation(),
 			QuoteAsset: newQuoteAssetKey(details.QuoteAsset).AsRelation(),
 		},
 	}
