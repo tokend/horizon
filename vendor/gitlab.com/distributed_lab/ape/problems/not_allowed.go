d55f48600a28313c638da52eabf2985656870578
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2019-03-20 20:46:58 +0200

    Bump vendor

--- vendor/gitlab.com/distributed_lab/ape/problems/not_allowed.go
+++ vendor/gitlab.com/distributed_lab/ape/problems/not_allowed.go
@@ -5,11 +5,41 @@ import (
 	"net/http"
 
 	"github.com/google/jsonapi"
+	"github.com/pkg/errors"
+	"gitlab.com/tokend/go/signcontrol"
 )
 
-func NotAllowed() *jsonapi.ErrorObject {
-	return &jsonapi.ErrorObject{
-		Title:  http.StatusText(http.StatusUnauthorized),
-		Status: fmt.Sprintf("%d", http.StatusUnauthorized),
+// NotAllowed will try to guess details of error and populate problem accordingly.
+func NotAllowed(errs ...error) *jsonapi.ErrorObject {
+	// errs is optional for backward compatibility
+	if len(errs) == 0 {
+		return &jsonapi.ErrorObject{
+			Title:  http.StatusText(http.StatusUnauthorized),
+			Status: fmt.Sprintf("%d", http.StatusUnauthorized),
+		}
 	}
+
+	if len(errs) != 1 {
+		panic(errors.New("unexpected number of errors passed"))
+	}
+
+	switch cause := errors.Cause(errs[0]); cause.(type) {
+	case *signcontrol.Error:
+		return &jsonapi.ErrorObject{
+			Title:  http.StatusText(http.StatusBadRequest),
+			Status: fmt.Sprintf("%d", http.StatusBadRequest),
+			Detail: "Request signature was invalid in some way",
+			Meta: &map[string]interface{}{
+				"reason": cause.Error(),
+			},
+		}
+	default:
+		// default sink for *doorman.Error and everything else,
+		// since 401 is our safe default
+		return &jsonapi.ErrorObject{
+			Title:  http.StatusText(http.StatusUnauthorized),
+			Status: fmt.Sprintf("%d", http.StatusUnauthorized),
+		}
+	}
+
 }
