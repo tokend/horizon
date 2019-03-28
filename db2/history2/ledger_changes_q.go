2a0e80ba36be2f6abb9ab623291587ff0001ae1e
Vlad <logvinenko.js@gmail.com>
2019-03-28 15:56:48 +0200

    Add ledger_changes and transactions q's

---
+++ db2/history2/ledger_changes_q.go
@@ -0,0 +1,46 @@
+package history2
+
+import (
+	sq "github.com/lann/squirrel"
+	"gitlab.com/distributed_lab/logan/v3/errors"
+	"gitlab.com/tokend/horizon/db2"
+)
+
+// LedgerChangesQ is a helper struct to aid in configuring queries that loads
+// ledger change structures.
+type LedgerChangesQ struct {
+	repo     *db2.Repo
+	selector sq.SelectBuilder
+}
+
+// NewLedgerChangesQ - creates new instance of LedgerChangesQ
+func NewLedgerChangesQ(repo *db2.Repo) LedgerChangesQ {
+	return LedgerChangesQ{
+		repo: repo,
+		selector: sq.Select(
+			"ledger_changes.tx_id",
+			"ledger_changes.op_id",
+			"ledger_changes.order_number",
+			"ledger_changes.effect",
+			"ledger_changes.entry_type",
+			"ledger_changes.payload",
+		).From("ledger_changes"),
+	}
+}
+
+// FilterByTransactionID - returns q with filter by transaction ID
+func (q LedgerChangesQ) FilterByTransactionID(id int64) LedgerChangesQ {
+	q.selector = q.selector.Where("ledger_changes.tx_id = ?", id)
+	return q
+}
+
+// Select - selects slice from the db, if no ledger changes found - returns nil, nil
+func (q LedgerChangesQ) Select() ([]LedgerChanges, error) {
+	var result []LedgerChanges
+	err := q.repo.Select(&result, q.selector)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to load ledger changes")
+	}
+
+	return result, nil
+}
