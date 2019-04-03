a3760c943bb2658c18fa1e40b72a017d4df385c0
Vlad <logvinenko.js@gmail.com>
2019-04-03 21:04:31 +0300

    Bump regources

---
+++ vendor/gitlab.com/tokend/regources/generated/model_order_book_relationships.go
@@ -0,0 +1,10 @@
+/*
+ * GENERATED. Do not modify. Your changes might be overwritten!
+ */
+
+package regources
+
+type OrderBookRelationships struct {
+	BuyEntries  *RelationCollection `json:"buy_entries,omitempty"`
+	SellEntries *RelationCollection `json:"sell_entries,omitempty"`
+}
