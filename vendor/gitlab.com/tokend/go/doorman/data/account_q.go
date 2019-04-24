c61d2197b4e8ed16b2567d9cf7dce92987306c7f
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2019-04-24 15:19:43 +0300

    Bump vendor

--- vendor/gitlab.com/tokend/go/doorman/data/account_q.go
+++ vendor/gitlab.com/tokend/go/doorman/data/account_q.go
@@ -2,6 +2,8 @@ package data
 
 import "gitlab.com/tokend/go/resources"
 
+//go:generate mockery -case underscore -name AccountQ -testonly  -outpkg=doorman -output=../
+
 // AccountQ data access interface for getting account details
 type AccountQ interface {
 	// Signers get account signers, nil slice is expected if account is not found
