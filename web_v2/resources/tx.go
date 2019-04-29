d933fe35ab3d25cd760eff773c0ad2e131e1d983
Oleksii Khramov <khramoff.al@gmail.com>
2019-04-29 20:06:29 +0300

    Transaction submit failure

--- web_v2/resources/tx.go
+++ web_v2/resources/tx.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 	"strconv"
 
+	"gitlab.com/distributed_lab/logan/v3/errors"
+	"gitlab.com/tokend/go/xdr"
+
 	"github.com/google/jsonapi"
 
 	"gitlab.com/tokend/horizon/txsub/v2"
@@ -19,15 +22,28 @@ func NewTxKey(txID int64) regources.Key {
 	}
 }
 
-func NewTxFailure(env txsub.EnvelopeInfo, err txsub.Error) error {
+func NewTxFailure(env txsub.EnvelopeInfo, txSubErr txsub.Error) error {
 	meta := make(map[string]interface{})
 	meta["envelope"] = env.RawBlob
-	meta["result_xdr"] = err.ResultXDR()
+	meta["result_xdr"] = txSubErr.ResultXDR()
+	var parsedResult xdr.TransactionResult
+	err := xdr.SafeUnmarshalBase64(txSubErr.ResultXDR(), &parsedResult)
+	if err != nil {
+		return errors.Wrap(err, "Failed to get parse tx result")
+	}
+
+	var parsedEnvelope xdr.TransactionEnvelope
+	err = xdr.SafeUnmarshalBase64(env.RawBlob, &parsedEnvelope)
+	if err != nil {
+		return errors.Wrap(err, "Failed to unmarshal tx envelope")
+	}
+
+	meta["parsed_envelope"] = parsedEnvelope
+	meta["parsed_result"] = parsedResult
 	return &jsonapi.ErrorObject{
-		Status: fmt.Sprintf("%d", err.Code()),
-		Detail: err.Details(),
-		Code:   http.StatusText(err.Code()),
-		Title:  "Transaction submit failed",
+		Status: fmt.Sprintf("%d", txSubErr.Code()),
+		Detail: txSubErr.Details(),
+		Title:  http.StatusText(txSubErr.Code()),
 		Meta:   &meta,
 	}
 }
