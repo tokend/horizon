93ccb48ae7291a24f568e486510884c98c984d2a
Vlad <logvinenko.js@gmail.com>
2019-05-08 14:21:19 +0300

    Add request

---
+++ web_v2/requests/get_match_list.go
@@ -0,0 +1,72 @@
+package requests
+
+import (
+	"gitlab.com/tokend/horizon/db2"
+	"net/http"
+)
+
+const (
+	// IncludeTypeMatchListBaseAsset - defines if base asset should be included in the response
+	IncludeTypeMatchListBaseAsset = "base_asset"
+	// IncludeTypeOrderBookQuoteAsset - defines if quote asset should be included in the response
+	IncludeTypeMatchListQuoteAsset = "quote_asset"
+
+	// FilterTypeMatchListBaseAsset - defines if we need to filter the list by base asset
+	FilterTypeMatchListBaseAsset = "base_asset"
+	// FilterTypeMatchListQuoteAsset - defines if we need to filter the list by quote asset
+	FilterTypeMatchListQuoteAsset = "quote_asset"
+	// FilterTypeMatchListOrderBook - defines if we need to filter the list by order book
+	FilterTypeMatchListOrderBook = "order_book"
+)
+
+var includeTypeMatchListAll = map[string]struct{}{
+	IncludeTypeMatchListBaseAsset:  {},
+	IncludeTypeMatchListQuoteAsset: {},
+}
+
+var filterTypeMatchListAll = map[string]struct{}{
+	FilterTypeMatchListBaseAsset:  {},
+	FilterTypeMatchListQuoteAsset: {},
+	FilterTypeMatchListOrderBook:  {},
+}
+
+// GetMatchList represents params to be specified by user for getMatchList handler
+type GetMatchList struct {
+	*base
+
+	Filters struct {
+		BaseAsset  string `fig:"base_asset,required"`
+		QuoteAsset string `fig:"quote_asset,required"`
+		OrderBook  int64  `fig:"order_book,required"`
+	}
+
+	PageParams *db2.CursorPageParams
+}
+
+// NewGetMatchList - returns new instance of GetMatchList
+func NewGetMatchList(r *http.Request) (*GetMatchList, error) {
+	b, err := newBase(r, baseOpts{
+		supportedIncludes: includeTypeMatchListAll,
+		supportedFilters:  filterTypeMatchListAll,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	pageParams, err := b.getCursorBasedPageParams()
+	if err != nil {
+		return nil, err
+	}
+
+	request := GetMatchList{
+		base:       b,
+		PageParams: pageParams,
+	}
+
+	err = b.populateFilters(&request.Filters)
+	if err != nil {
+		return nil, err
+	}
+
+	return &request, nil
+}
