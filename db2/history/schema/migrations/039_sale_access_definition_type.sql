7811c1d6b7c5bcd53c5e729038c08f7db5b8302a
Vlad <logvinenko.js@gmail.com>
2019-05-20 14:48:58 +0300

    Add default value for sale access definition type

--- db2/history/schema/migrations/039_sale_access_definition_type.sql
+++ db2/history/schema/migrations/039_sale_access_definition_type.sql
@@ -4,4 +4,4 @@ alter table sales add column access_definition_type int not null;
 
 -- +migrate Down
 
-alter table sales drop column if exists access_definition_type;
+alter table sales drop column if exists access_definition_type default 0;
