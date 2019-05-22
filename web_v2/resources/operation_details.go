a6ad1c271a8ef015b685990a0b33d332f9d518c4
Oleksii Khramov <khramoff.al@gmail.com>
2019-05-22 18:34:32 +0300

    Fixed panic on operation details inclusion in GetHistory

--- web_v2/resources/operation_details.go
+++ web_v2/resources/operation_details.go
@@ -107,6 +107,8 @@ func NewOperationDetails(op history2.Operation) regources.Resource {
 		return newManagePollOp(op.ID, *op.Details.ManagePoll)
 	case xdr.OperationTypeManageVote:
 		return newManageVoteOp(op.ID, *op.Details.ManageVote)
+	case xdr.OperationTypeManageAccountSpecificRule:
+		return newManageAccountSpecificRule(op.ID, *op.Details.ManageAccountSpecificRule)
 	default:
 		panic(errors.From(errors.New("unexpected operation type"), logan.F{
 			"type": op.Type,
@@ -572,4 +574,28 @@ func newStampOp(id int64, details history2.StampDetails) *regources.StampOp {
 			LicenseHash: details.LicenseHash,
 		},
 	}
+}
+
+func newManageAccountSpecificRule(id int64, details history2.ManageAccountSpecificRuleDetails) *regources.ManageAccountSpecificRuleOp {
+	manageAccSpecificRuleOp := &regources.ManageAccountSpecificRuleOp{
+		Key: regources.NewKeyInt64(id, regources.OPERATIONS_MANAGE_ACCOUNT_SPECIFIC_RULE),
+		Attributes: &regources.ManageAccountSpecificRuleOpAttributes{
+			Action: details.Action,
+		},
+		Relationships: &regources.ManageAccountSpecificRuleOpRelationships{
+			Rule: regources.NewKeyInt64(int64(details.RuleID), regources.ACCOUNT_SPECIFIC_RULES).AsRelation(),
+		},
+	}
+	switch details.Action {
+	case xdr.ManageAccountSpecificRuleActionCreate:
+		manageAccSpecificRuleOp.Attributes.CreateData = &regources.CreateAccountSpecificRuleData{
+			Forbids:   details.CreateDetails.Forbids,
+			LedgerKey: details.CreateDetails.LedgerKey,
+		}
+		if details.CreateDetails.AccountID != "" {
+			manageAccSpecificRuleOp.Attributes.CreateData.AccountId = &details.CreateDetails.AccountID
+		}
+	case xdr.ManageAccountSpecificRuleActionRemove:
+	}
+	return manageAccSpecificRuleOp
 }
