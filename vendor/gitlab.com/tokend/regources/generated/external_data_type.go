5de3186ce2e1f2cdd0f91f36a483f7c013f1a2dd
Oleksii Khramov <khramoff.al@gmail.com>
2019-06-28 20:05:35 +0300

    Switched from flag to plain string

--- vendor/gitlab.com/tokend/regources/generated/external_data_type.go
+++ vendor/gitlab.com/tokend/regources/generated/external_data_type.go
@@ -4,39 +4,9 @@
 
 package regources
 
-import "encoding/json"
-
-type ExternalDataType int
+type ExternalDataType string
 
 const (
-	ExternalDataTypeAddress ExternalDataType = iota + 1
-	ExternalDataTypeAddressWithPayload
+	ExternalDataTypeAddress            ExternalDataType = "address"
+	ExternalDataTypeAddressWithPayload                  = "address_with_payload"
 )
-
-var externalDataTypeMap = map[ExternalDataType]string{
-	ExternalDataTypeAddress:            "address",
-	ExternalDataTypeAddressWithPayload: "address_with_payload",
-}
-
-func (s ExternalDataType) MarshalJSON() ([]byte, error) {
-	return json.Marshal(Flag{
-		Name:  externalDataTypeMap[s],
-		Value: int32(s),
-	})
-}
-
-//String - converts int enum to string
-func (s ExternalDataType) String() string {
-	return externalDataTypeMap[s]
-}
-
-func (s *ExternalDataType) UnmarshalJSON(b []byte) error {
-	var res Flag
-	err := json.Unmarshal(b, &res)
-	if err != nil {
-		return err
-	}
-
-	*s = ExternalDataType(res.Value)
-	return nil
-}
--- vendor/gitlab.com/tokend/regources/generated/model_external_system_data.go
+++ vendor/gitlab.com/tokend/regources/generated/model_external_system_data.go
@@ -4,8 +4,45 @@
 
 package regources
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+
+	"gitlab.com/distributed_lab/logan/v3/errors"
+)
+
 type ExternalSystemData struct {
 	Data ExternalSystemDataEntry `json:"data"`
-	// Possible values: * 1 - address * 2 - address_with_payload
+	// Possible values: * address * address_with_payload
 	Type ExternalDataType `json:"type"`
+}
+
+//Value - implements db driver method for auto marshal
+func (r ExternalSystemData) Value() (driver.Value, error) {
+	result, err := json.Marshal(r)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal ExternalDataType data")
+	}
+
+	return result, nil
+}
+
+//Scan - implements db driver method for auto unmarshal
+func (r *ExternalSystemData) Scan(src interface{}) error {
+	var data []byte
+	switch rawData := src.(type) {
+	case []byte:
+		data = rawData
+	case string:
+		data = []byte(rawData)
+	default:
+		return errors.New("Unexpected type for jsonb")
+	}
+
+	err := json.Unmarshal(data, r)
+	if err != nil {
+		return errors.Wrap(err, "failed to unmarshal ExternalSystemData data")
+	}
+
+	return nil
 }
