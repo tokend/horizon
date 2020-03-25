bfebb5b4bc7cde17e91037b0f88be329bf976a99
Viktor Dzenkiv <viktor.dzenkiv@nure.ua>
2020-03-25 15:45:46 +0000

    Fix/invalid conversion price

--- web_v2/handlers/balance_state_converter.go
+++ web_v2/handlers/balance_state_converter.go
@@ -2,11 +2,12 @@ package handlers
 
 import (
 	"gitlab.com/distributed_lab/logan/v3/errors"
+	"gitlab.com/tokend/go/amount"
 	"gitlab.com/tokend/horizon/db2"
 	"gitlab.com/tokend/horizon/db2/core2"
 	"gitlab.com/tokend/horizon/exchange"
 	"gitlab.com/tokend/horizon/web_v2/resources"
-	"gitlab.com/tokend/regources/generated"
+	regources "gitlab.com/tokend/regources/generated"
 )
 
 // balanceStateConverter - helper struct to convert balance states to different assets
@@ -65,18 +66,48 @@ func (c *balanceStateConverter) Convert(balance core2.Balance, toAsset string) (
 			balance,
 			regources.Amount(0),
 			regources.Amount(0),
+			regources.Amount(0),
 			false,
 		)
 
 		return &convertedState, nil
 	}
 
+	price, err := getPrice(*convertedAvailable, *convertedLocked, int64(balance.Amount), int64(balance.Locked))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get price")
+	}
+
 	convertedState := resources.NewConvertedBalanceState(
 		balance,
 		regources.Amount(*convertedAvailable),
 		regources.Amount(*convertedLocked),
+		regources.Amount(price),
 		true,
 	)
 
 	return &convertedState, nil
+}
+
+// minimum amount is always 1  because method GetMinimumAmount() returns 10^N where N is 0
+func getPrice(convertedAvailable, convertedLocked, balanceAmount, balanceLocked int64) (int64, error) {
+	if (convertedAvailable != 0) && (balanceAmount != 0) {
+		return get(convertedAvailable, balanceAmount, 1)
+	}
+
+	if (convertedAvailable == 0) &&
+		(convertedLocked != 0) &&
+		(balanceLocked != 0) {
+		return get(convertedLocked, balanceLocked, 1)
+	}
+
+	return 0, nil
+}
+
+func get(quote, base, minimumAmount int64) (int64, error) {
+	price, isOverflow := amount.BigDivide(quote, amount.One, base, amount.ROUND_UP, minimumAmount)
+	if isOverflow {
+		return 0, errors.New("price is overflow")
+	}
+	return price, nil
 }
