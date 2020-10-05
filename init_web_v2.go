5a18900675d37030bec6fb8972675aeb753ef4b5
Yaroslav Panasenko <yar.panasenko@gmail.com>
2020-10-06 00:55:33 +0300

    Add endpoints for close deferred payments requests

--- init_web_v2.go
+++ init_web_v2.go
@@ -308,6 +308,9 @@ func initWebV2Actions(app *App) {
 	m.Get("/v3/create_deferred_payments_requests", handlers.GetCreateDeferredPaymentRequests)
 	m.Get("/v3/create_deferred_payments_requests/{id}", handlers.GetCreateDeferredPaymentRequests)
 
+	m.Get("/v3/create_close_deferred_payments_requests", handlers.GetCloseDeferredPaymentRequests)
+	m.Get("/v3/create_close_deferred_payments_requests/{id}", handlers.GetCloseDeferredPaymentRequests)
+
 	m.Get("/v3/data_update_requests", handlers.GetDataUpdateRequests)
 	m.Get("/v3/data_update_requests/{id}", handlers.GetDataUpdateRequests)
 
