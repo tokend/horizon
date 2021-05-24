605bd42659fbf72300189d430a7c620faad296b3
Skriabin <artem.skriabin@distributedlab.com>
2021-05-24 15:24:30 +0300

    Update pg-entrypoint.sh according to debug/muna branch version

--- pg-entrypoint.sh
+++ pg-entrypoint.sh
@@ -2,7 +2,10 @@
 set -eax
 
 POSTGRES_PORT=${POSTGRES_PORT:-5432}
-pg-entrypoint.sh "postgres -p $POSTGRES_PORT" &
+mkdir -p /var/pg_dlogs
+touch /var/pg_dlogs/postgresql.log
+chmod -R 777 /var/pg_dlogs
+pg-entrypoint.sh "postgres -p $POSTGRES_PORT -c logging_collector=on -c log_directory=/var/pg_dlogs -c log_filename=postgresql.log -c log_statement=all" &
 
 while ! pg_isready -U$POSTGRES_USER -hlocalhost -p$POSTGRES_PORT > /dev/null 2> /dev/null; do
     echo "postgres not ready yet"
