41dc3f0fc90f74ab71d19eca8446859cd984e04a
zh0glikk <zhogliksasha3434@gmail.com>
2021-08-30 17:23:32 +0300

    small fix

--- web_v2/handlers/get_create_withdraw_requests.go
+++ web_v2/handlers/get_create_withdraw_requests.go
@@ -39,6 +39,7 @@ func GetCreateWithdrawRequests(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	ctx.Log(r).Info(request.Filters.Asset)
 	err = handler.MakeAll(w, request)
 	if err != nil {
 		ctx.Log(r).WithError(err).Error("failed to get request list", logan.F{
